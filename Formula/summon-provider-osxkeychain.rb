f6dbbf88396065bb89feeeb02c14521bf2114b78
Mithun Ayachit <m0t0rbr3th@gmail.com>
2020-04-03 11:18:32 -0500

    summon-provider-osxkeychain-v0.3.0

--- Formula/summon-provider-osxkeychain.rb
+++ Formula/summon-provider-osxkeychain.rb
@@ -1,8 +1,8 @@
 class SummonProviderOsxkeychain < Formula
   desc "Summon provider to manage secrets in macOS Keychain"
   homepage "https://github.com/mithun/summon-provider-osxkeychain"
-  url "https://github.com/mithun/summon-provider-osxkeychain/archive/v0.2.0.tar.gz"
-  sha256 "a15a30e1365053c2216cbe60c5e0aa70042130d46c95ab8cb845abea1d2a0eee"
+  url "https://github.com/mithun/summon-provider-osxkeychain/archive/v0.3.0.tar.gz"
+  sha256 "a7efd0d750e7a36dde51e525f0007841d19db146bcc0e02477f47964e166ba3f"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"

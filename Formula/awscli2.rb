3fcf62460f56b32138969008f2f19297cb3a3bd1
Mithun Ayachit <m0t0rbr3th@gmail.com>
2020-09-23 07:56:43 -0500

    add awscli2

---
+++ Formula/awscli2.rb
@@ -0,0 +1,13 @@
+cask "awscli2" do
+  name "AWS Command Line Interface"
+  homepage "https://aws.amazon.com/cli/"
+  version "2.0.50"
+  sha256 "9df5e8d901c8d1f24a052bc745966ec52809815f05cddce690e8680f11a79bf9"
+  url "https://awscli.amazonaws.com/AWSCLIV2-#{version}.pkg"
+
+  depends_on macos: ">= :mojave"
+
+  pkg "AWSCLIV2-#{version}"
+
+  uninstall pkgutil: "com.amazon.aws.cli.*"
+end

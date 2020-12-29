78567faa9c9602754a5ca5896529b800bdf37437
Mithun Ayachit <m0t0rbr3th@gmail.com>
2020-12-29 09:18:46 -0600

    bump awscli2 to v2.1.14

--- Casks/awscli2.rb
+++ Casks/awscli2.rb
@@ -1,8 +1,8 @@
 cask "awscli2" do
   name "AWS Command Line Interface"
   homepage "https://aws.amazon.com/cli/"
-  version "2.1.1"
-  sha256 "c22c2790a756a0a399f1b6c346e59c76beea58e0fb7154d1000ff5192d030b71"
+  version "2.1.14"
+  sha256 "74ad9760d08d6044d90784e2bded943521a34833e71c21c7d851c987a3e4fc97"
   url "https://awscli.amazonaws.com/AWSCLIV2-#{version}.pkg"
 
   depends_on macos: ">= :mojave"

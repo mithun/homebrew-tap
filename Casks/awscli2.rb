129bdc01a9ac11d75282768f4713f198b9bec54d
Mithun Ayachit <mithun@users.noreply.github.com>
2021-01-04 07:06:53 -0600

    bump awscliv2 to 2.1.15

--- Casks/awscli2.rb
+++ Casks/awscli2.rb
@@ -1,15 +1,13 @@
+cask "awscli2" do
+    name "AWS Command Line Interface"
+    homepage "https://aws.amazon.com/cli/"
+    version "2.1.15"
+    sha256 "e30d855c1cfb09bc532182800401ccc7e39a02ec8dfbc9ae196c7d45de6da15e"
+    url "https://awscli.amazonaws.com/AWSCLIV2-2.1.15.pkg"
 
-    cask "awscli2" do
-        name "AWS Command Line Interface"
-        homepage "https://aws.amazon.com/cli/"
-        version "2.1.15"
-        sha256 "e30d855c1cfb09bc532182800401ccc7e39a02ec8dfbc9ae196c7d45de6da15e"
-        url "https://awscli.amazonaws.com/AWSCLIV2-2.1.15.pkg"
+    depends_on macos: ">= :mojave"
 
-        depends_on macos: ">= :mojave"
+    pkg "AWSCLIV2-2.1.15.pkg"
 
-        pkg "AWSCLIV2-2.1.15.pkg"
-
-        uninstall pkgutil: "com.amazon.aws.cli.*"
-    end
-    
+    uninstall pkgutil: "com.amazon.aws.cli.*"
+end
